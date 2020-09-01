afe886ca7401ce1421e9af5b59d5817589f5564c
Milenakamine <aakaminemilena@gmail.com>
2020-09-01 16:50:13 -0300

    Finalizado

---
+++ API_Boletim/Interfaces/IAluno.cs
@@ -0,0 +1,22 @@
+﻿using API_Boletim.Domains;
+using System.Collections.Generic;
+
+namespace API_Boletim.Interfaces
+{
+    interface IAluno
+    {
+        Aluno Cadastrar(Aluno a);
+        
+        List<Aluno> LerTodos();
+
+        Aluno BuscarPorId(int id);
+
+        Aluno Alterar(Aluno a);
+
+        Aluno Ecluir(Aluno a);
+
+
+
+
+    }
+}
